c7159dba37af5c60110683ef6ea9000bf62c7c91
formazione <gatto.gio@gmail.com>
2020-12-10 10:50:15 +0100

    one class

--- pydarm2.py
+++ pydarm2.py
@@ -8,50 +8,14 @@ from files.darm_end import *
 # from dati.browser3 import *
 
 
-
-
-
-def create_list_of_dict(filename):
-	"return dictionary with question.... image...."
-
-	qdic = {}
-	flist = []
-	def list_of_questions():
-		# read the file and splits every qna in a string
-		# file is a list of strings each with a qna
-		with open(filename, 'r', encoding='utf-8') as file:
-			file = file.read()
-			file = file.split("\n\n")
-		# every string in the list becomes a list with [q,a,a...]
-		return file
-
-	def list_of_list_with_qna(file):
-		for eachstring in file:
-			flist.append(eachstring.split("\n"))
-
-	def delete_empty_items(): 
-		for eachsublist in flist:
-			for e in eachsublist:
-				if e == '':
-					eachsublist.pop(eachsublist.index(e))
-			question = eachsublist[0]
-			eachsublist.pop(0)
-			qdic[question] = eachsublist
-	file = list_of_questions()
-	list_of_list_with_qna (file)
-	delete_empty_items()
-
-	return qdic
-
-
 class Start:
 
 	def __init__(self):
 		"a menu to choose a file in the directory 'dati'"
 		self.images()
 		self.show_files()
-		self.inputfilename()
-		self.create_code_for_template()
+		self.inputfilename() # self.filename contains the txt file with questions you choose
+		self.create_list_of_dict(self.filename)
 		self.create_test()
 
 	def images(self):
@@ -59,8 +23,58 @@ class Start:
 			"https://picsum.photos/200"
 			]
 
-	def create_code_for_template(self):
-		self.qdic = create_list_of_dict(self.filename)
+	def create_list_of_dict(self, filename):
+		"takes the data from filename and convert them into a dictonary"
+
+		'''
+		from
+
+		what is...
+		is ...
+		is ...
+		is ...
+		
+		to this
+
+		{
+		question = "What is....",
+		image = "",
+		choice = ["is...", "is...", "is..."],
+		correct = "",
+		explation = ""
+		}
+
+
+		'''
+
+		qdic = {}
+		flist = []
+		def list_of_questions():
+			# read the file and splits every qna in a string
+			# file is a list of strings each with a qna
+			with open(filename, 'r', encoding='utf-8') as file:
+				file = file.read()
+				file = file.split("\n\n")
+			# every string in the list becomes a list with [q,a,a...]
+			return file
+
+		def list_of_list_with_qna(file):
+			for eachstring in file:
+				flist.append(eachstring.split("\n"))
+
+		def delete_empty_items(): 
+			for eachsublist in flist:
+				for e in eachsublist:
+					if e == '':
+						eachsublist.pop(eachsublist.index(e))
+				question = eachsublist[0]
+				eachsublist.pop(0)
+				qdic[question] = eachsublist
+		file = list_of_questions()
+		list_of_list_with_qna (file)
+		delete_empty_items()
+
+		self.qdic = qdic
 
 	def show_files(self):
 		text = "File di testo nella cartella: dati"
@@ -79,7 +93,8 @@ class Start:
 			"question"      :   "{question}",
 			"image"         :   "{choice(self.imgslist)}",
 			"choices"       :   {answers},
-			"correct"       :   "{answers[0]}", "explanation"   :   "", }},"""
+			"correct"       :   "{answers[0]}",
+			"explanation"   :   "", }},"""
 		return html
 
 	def create_test(self):
